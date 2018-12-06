01ff9f6799170074e37bc4fec3582d0805e05998
covet <HaniLucky@163.com>
2018-12-06 08:01:36 +0800

    my-project添加文档

--- myproject/my-project/src/main/java/com/example/my/project/controller/DepController.java
+++ myproject/my-project/src/main/java/com/example/my/project/controller/DepController.java
@@ -5,6 +5,7 @@ import com.example.my.project.service.DepService;
 import com.example.my.project.vo.Dep;
 import org.apache.ibatis.annotations.ResultMap;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.List;
 /**
  * Created by Covet on 2018/12/5.
  */
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/dep")
 public class DepController {
